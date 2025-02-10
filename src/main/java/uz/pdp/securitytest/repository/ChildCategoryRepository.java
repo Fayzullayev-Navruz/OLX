861f50059a3fc13fd767dda889526a0e279d52a0
navruz_fayzullaev <Navruz_Fayzullaev@student.itpu.uz>
2025-02-10 19:41:49 +0500

    child category product ga otyapti

--- src/main/java/uz/pdp/securitytest/repository/ChildCategoryRepository.java
+++ src/main/java/uz/pdp/securitytest/repository/ChildCategoryRepository.java
@@ -3,6 +3,11 @@ package uz.pdp.securitytest.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import uz.pdp.securitytest.entity.ChildCategory;
+
+import java.util.List;
+
 @Repository
 public interface ChildCategoryRepository extends JpaRepository<ChildCategory,Integer> {
+    List<ChildCategory> findByCategory_Id(Integer id);
+
 }
